02d1ed7f7034cd3928c4cbf91d266017c5f9436e
Vishal Shetagar <vishal.shetegar1@gmail.com>
2026-06-02 22:04:29 +0530

    refactor: implement global workspace profile scoping and remove unused device shadow page

--- frontend/src/routes/App.tsx
+++ frontend/src/routes/App.tsx
@@ -12,7 +12,6 @@ import { LandingPage } from "../pages/LandingPage";
 import { ObservabilityPage } from "../pages/ObservabilityPage";
 import { OTAPage } from "../pages/OTAPage";
 import { SettingsPage } from "../pages/SettingsPage";
-import { ShadowPage } from "../pages/ShadowPage";
 import { WorkspacesPage } from "../pages/WorkspacesPage";
 import { SessionDashboardPage } from "../pages/SessionDashboardPage";
 import { TelemetryPage } from "../pages/TelemetryPage";
@@ -33,7 +32,6 @@ export function App() {
         <Route path="dashboard" element={<DashboardPage />} />
         <Route path="telemetry" element={<TelemetryPage />} />
         <Route path="devices" element={<DevicesPage />} />
-        <Route path="shadow" element={<ShadowPage />} />
         <Route path="ota" element={<OTAPage />} />
         <Route path="commands" element={<CommandsPage />} />
         <Route path="alerts" element={<AlertsPage />} />
