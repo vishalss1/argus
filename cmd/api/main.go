bd59f7310f6bc2f87ca340191b0bc5940398e8de
Vishal Shetagar <vishal.shetegar1@gmail.com>
2026-06-11 11:21:06 +0530

    chore: remove pprof profiling server and unused imports

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"log"
-	"net/http"
-	_ "net/http/pprof"
 	"os"
 	"os/signal"
 	"syscall"
@@ -24,15 +22,6 @@ func main() {
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 
-	// Start pprof HTTP server for profiling
-	pprofServer := &http.Server{Addr: ":6060", Handler: nil}
-	go func() {
-		log.Printf("[PPROF] starting pprof server on :6060")
-		if err := pprofServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Printf("[PPROF] pprof server error: %v", err)
-		}
-	}()
-
 	go func() {
 		if err := server.Start(); err != nil {
 			log.Printf("server stopped: %v", err)
@@ -42,7 +31,6 @@ func main() {
 	sig := <-sigChan
 	log.Printf("Received termination signal %v, shutting down gracefully...", sig)
 	
-	pprofServer.Close()
 	if err := server.Close(); err != nil {
 		log.Fatalf("server shutdown failed: %v", err)
 	}
